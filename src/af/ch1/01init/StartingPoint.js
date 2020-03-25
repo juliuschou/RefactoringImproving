d89556f8fe1f9ed2f018b5f611853558f27f9b79
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 19:12:07 +0800

    apply Change Function Declaration (124) to amountFor to remove the play parameter - (3)

--- src/af/ch1/01init/StartingPoint.js
+++ src/af/ch1/01init/StartingPoint.js
@@ -55,7 +55,7 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }                 
 
-    function amountFor(aPerformance, play){
+    function amountFor(aPerformance){
         let result = 0;
         switch (playFor(aPerformance).type) {
             case 'tragedy':
