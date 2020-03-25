71d2cdda122bda2ad9e72e502faae165d946a77f
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 22:15:10 +0800

    *Moving Functions into the Calculator - volumeCreditsFor[move, delegating and call]

--- src/af/ch1/03reOrgCalcByType/createStatementData.js
+++ src/af/ch1/03reOrgCalcByType/createStatementData.js
@@ -25,18 +25,9 @@ export default function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
-    
-    function volumeCreditsFor(aPerformance){
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-
-        if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-
-        return result;
-    }       
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
@@ -48,6 +39,15 @@ class PerformanceCalculator {
         this.performance = aPerformance;
         this.play = aPlay;
     }      
+
+    get volumeCredits(){
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+
+        if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
+
+        return result;
+    }    
 
     get amount(){
         let result = 0;
