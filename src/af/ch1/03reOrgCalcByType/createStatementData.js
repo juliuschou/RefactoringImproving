6f267f1b8500994e7246cbda344ee67a4bf40a3f
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 22:11:59 +0800

    *Moving Functions into the Calculator - amountFor (2) use Inline Function (115) to call the new function directly

--- src/af/ch1/03reOrgCalcByType/createStatementData.js
+++ src/af/ch1/03reOrgCalcByType/createStatementData.js
@@ -24,7 +24,7 @@ export default function createStatementData(invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
@@ -41,9 +41,7 @@ export default function createStatementData(invoice, plays) {
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }  
-    function amountFor(aPerformance){
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-    }    
+
 }
 class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
