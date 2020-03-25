4717e466b8727899da7ecd34d288bfb892cf6dc0
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 20:17:13 +0800

    Removing Total Volume Credits - replace temp with query (1) Extraction Function

--- src/af/ch1/01init/StartingPoint.js
+++ src/af/ch1/01init/StartingPoint.js
@@ -38,14 +38,20 @@ function statement(invoice, plays) {
         totalAmount += amountFor(perf);
     }
 
-    let volumeCredits = 0;
-    for (let perf of invoice['performances']) {      
-        volumeCredits += volumeCreditsFor(perf);        
-    }
+    let volumeCredits = totalVolumeCredits();
 
     result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+
+
+    function totalVolumeCredits(){
+        let volumeCredits = 0;
+        for (let perf of invoice['performances']) {      
+            volumeCredits += volumeCreditsFor(perf);        
+        }
+        return volumeCredits;
+    }
 
     function usd(aNumber){
         //const format = new Intl.NumberFormat("en-­US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
