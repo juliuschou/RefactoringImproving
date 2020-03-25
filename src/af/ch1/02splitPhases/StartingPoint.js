58fa897136b8e1a7aab770ede65f4d29c7ed2a3f
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 20:50:27 +0800

    take the customer and add it to the intermediate object
    I now examine the other arguments used by renderPlainText. I want to move the data that comes from them into the intermediate data structure, so that all the calculation code moves into the statement function and renderPlainText operates solely on data passed to it through the data parameter. My first move is to take the customer and add it to the intermediate object

--- src/af/ch1/02splitPhases/StartingPoint.js
+++ src/af/ch1/02splitPhases/StartingPoint.js
@@ -29,11 +29,12 @@ console.log(rlt);
 
 function statement(invoice, plays) {
     const statementData = {};
+    statementData.customer = invoice.customer;
     return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
+    let result = `Statement for ${data.customer}\n`;
 
     for (let perf of invoice['performances']) {       
         // print line for this order
