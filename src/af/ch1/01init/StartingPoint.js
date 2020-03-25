e8cc9439a7e5278d55e4fa443fb0a913d54920d9
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 20:26:26 +0800

    Removing totalAmount - Split loop (2) Identify and eliminate duplicate side effects

--- src/af/ch1/01init/StartingPoint.js
+++ src/af/ch1/01init/StartingPoint.js
@@ -31,20 +31,15 @@ function statement(invoice, plays) {
     let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
-    for (let perf of invoice['performances']) {
-       
+    for (let perf of invoice['performances']) {       
         // print line for this order
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    }
+
+    for (let perf of invoice['performances']) {       
         totalAmount += amountFor(perf);
     }
 
-    for (let perf of invoice['performances']) {
-       
-        // print line for this order
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
-    }
-        
     result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
