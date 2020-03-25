2db56dbd350f7466464aebe46131efd150bd67b9
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 19:55:17 +0800

    *Extracting Volume Credits - -author's coding standard

--- src/af/ch1/01init/StartingPoint.js
+++ src/af/ch1/01init/StartingPoint.js
@@ -49,12 +49,12 @@ function statement(invoice, plays) {
 
 
     function volumeCreditsFor(perf){
-        let volumeCredits = 0;
-        volumeCredits += Math.max(perf.audience - 30, 0);
+        let result = 0;
+        result += Math.max(perf.audience - 30, 0);
 
-        if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        if ('comedy' === playFor(perf).type) result += Math.floor(perf.audience / 5);
 
-        return volumeCredits;
+        return result;
     }     
 
     function playFor(aPerformance) {
