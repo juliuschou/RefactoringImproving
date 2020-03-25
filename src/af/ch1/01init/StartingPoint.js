0fd5f47b31a34ce256ac6e163dbc6af336dbe73e
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 19:49:09 +0800

    *Extracting Volume Credits - copy code
    Copy the extracted code from the source function into the new target function

--- src/af/ch1/01init/StartingPoint.js
+++ src/af/ch1/01init/StartingPoint.js
@@ -52,7 +52,12 @@ function statement(invoice, plays) {
 
 
     function volumeCreditsFor(){
+        let volumeCredits = 0;
+        volumeCredits += Math.max(perf.audience - 30, 0);
 
+        if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+
+        return volumeCredits;
     }     
 
     function playFor(aPerformance) {
