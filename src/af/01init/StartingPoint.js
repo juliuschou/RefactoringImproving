343099e07a179efdf0ffab136ebf7f1e2a551721
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 18:21:04 +0800

    extract amountFor - copy code
    Copy the extracted code from the source function into the new target function

--- src/af/01init/StartingPoint.js
+++ src/af/01init/StartingPoint.js
@@ -70,5 +70,23 @@ function statement(invoice, plays) {
 }
 
 function amountFor(){
+    let thisAmount = 0;
+    switch (play.type) {
+        case 'tragedy':
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case 'comedy':
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
 
 }
