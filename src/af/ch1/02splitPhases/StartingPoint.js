2ba0899aab6e6567123b2c63fd6918993b78decf
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 20:47:39 +0800

    create an object that will act as my intermediate data structure between the two phases.
    I pass this data object in as an argument to renderPlainText

--- src/af/ch1/02splitPhases/StartingPoint.js
+++ src/af/ch1/02splitPhases/StartingPoint.js
@@ -28,10 +28,11 @@ let rlt = statement(invoices[0], plays);
 console.log(rlt);
 
 function statement(invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice['performances']) {       
