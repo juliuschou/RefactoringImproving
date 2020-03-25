ae85eeaa5eaa8c6d8abe904f8bd870050ef11625
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 21:18:39 +0800

    add the amount for each performance in intermediate data

--- src/af/ch1/02splitPhases/StartingPoint.js
+++ src/af/ch1/02splitPhases/StartingPoint.js
@@ -36,20 +36,43 @@ function statement(invoice, plays) {
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
+        result.amount = amountFor(result);
         return result;
     }
     
+    function amountFor(aPerformance){
+        let result = 0;
+        switch (aPerformance.play.type) {
+            case 'tragedy':
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case 'comedy':
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${perf.play.type}`);
+        }
+        return result;
+    }
+
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }  
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;
 
     for (let perf of data['performances']) {       
         // print line for this order
-        result += ` ${perf.play.name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += ` ${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience} seats)\n`;
     }
 
 
@@ -60,7 +83,7 @@ function renderPlainText(data, plays) {
     function totalAmount(){
         let result = 0;
         for (let perf of data['performances']) {       
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result;
     }    
@@ -87,26 +110,6 @@ function renderPlainText(data, plays) {
         return result;
     }               
 
-    function amountFor(aPerformance){
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case 'tragedy':
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case 'comedy':
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${perf.play.type}`);
-        }
-        return result;
-    }
+ 
 }
 
