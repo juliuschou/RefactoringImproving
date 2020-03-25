8666fc74739e2244f41dfc5791c0ce6b83f40cab
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-25 21:25:16 +0800

    move the two calculations of the totals.

--- src/af/ch1/02splitPhases/StartingPoint.js
+++ src/af/ch1/02splitPhases/StartingPoint.js
@@ -31,8 +31,27 @@ function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
-    return renderPlainText(statementData, plays);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+   return renderPlainText(statementData, plays);
     
+
+    function totalAmount(data){
+        let result = 0;
+        for (let perf of data['performances']) {       
+            result += perf.amount;
+        }
+        return result;
+    }    
+
+    function totalVolumeCredits(data){
+        let result = 0;
+        for (let perf of data['performances']) {      
+            result += perf.volumeCredits;        
+        }
+        return result;
+    }
+
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
@@ -85,25 +104,9 @@ function renderPlainText(data) {
     }
 
 
-    result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
+    result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
+    result += `You earned ${data.totalVolumeCredits} credits\n`;
     return result;
-
-    function totalAmount(){
-        let result = 0;
-        for (let perf of data['performances']) {       
-            result += perf.amount;
-        }
-        return result;
-    }    
-
-    function totalVolumeCredits(){
-        let result = 0;
-        for (let perf of data['performances']) {      
-            result += perf.volumeCredits;        
-        }
-        return result;
-    }
 
     function usd(aNumber){
         //const format = new Intl.NumberFormat("en-­US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
